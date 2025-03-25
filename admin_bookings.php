15c8cc5772fc744ddfd15bb1239ded552a38bb44
k2rt23 <karttriin@gmail.com>
2025-03-25 20:03:49 +0200

    Move admin_bookings.php to admin folder and update relative paths

--- admin_bookings.php
+++ admin_bookings.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
+
+if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'admin') {
+    header("Location: index.php"); 
     exit();
 }
 
