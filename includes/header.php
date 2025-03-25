9fe525bf71f0fb28e60b3df23cf20a9d7a28478d
k2rt23 <karttriin@gmail.com>
2025-03-25 20:03:34 +0200

    Update navigation menu to show links based on user role (login/logout, admin/user)

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,18 @@
+<?php session_start(); ?>
 <header>
     <nav>
         <a href="index.php">Avaleht</a>
         <a href="trainings.php">Treeningud</a>
         <a href="contact.php">Kontakt</a>
-    </nav>
-</header>
+
+        <?php if (!isset($_SESSION['user_id'])): ?>
+            <a href="login.php">Logi sisse</a>
+            <a href="register.php">Registreeru</a>
+            <?php else: ?>
+    <?php if ($_SESSION['user_role'] === 'admin'): ?>
+        <a href="admin_bookings.php">Admin ala</a>
+    <?php else: ?>
+        <a href="my_bookings.php">Minu broneeringud</a>
+    <?php endif; ?>
+    <a href="logout.php">Logi välja</a>
+<?php endif; ?>   
