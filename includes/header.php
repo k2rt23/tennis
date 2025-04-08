7cfd33703d2644f340f2a491243f6f53078f18f1
k2rt23 <karttriin@gmail.com>
2025-04-08 15:29:19 +0300

    Add conditional navigation for logged-in and guest users

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,16 @@
-<?php session_start(); ?>
+<?php if (session_status() === PHP_SESSION_NONE) { session_start(); } ?>
 <header>
     <nav>
         <a href="index.php">Avaleht</a>
         <a href="trainings.php">Treeningud</a>
         <a href="contact.php">Kontakt</a>
 
-        <?php if (!isset($_SESSION['user_id'])): ?>
-            <a href="login.php">Logi sisse</a>
-            <a href="register.php">Registreeru</a>
-            <?php else: ?>
-    <?php if ($_SESSION['user_role'] === 'admin'): ?>
-        <a href="admin_bookings.php">Admin ala</a>
-    <?php else: ?>
+        <?php if (isset($_SESSION['user_id'])): ?>
         <a href="my_bookings.php">Minu broneeringud</a>
+        <a href="logout.php">Logi välja</a>
+    <?php else: ?>
+        <a href="login.php">Logi sisse</a>
+        <a href="register.php">Registreeru</a>
     <?php endif; ?>
-    <a href="logout.php">Logi välja</a>
-<?php endif; ?>   
+</nav>
+    </header>
